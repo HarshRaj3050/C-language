8becb53b03113720e0ac2f6f194638dadcbcc7a9
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-04 17:24:41 +0530

    question update

--- Structure/01_Question.c
+++ Structure/01_Question.c
@@ -13,24 +13,24 @@ struct employee
 
 int main()
 {
-    struct employee e1[3];
+    struct employee e[3];
     for (int i = 1; i < 4; i++)
     {
         printf("%d. Enter Employee details\n\n", i);
         
         printf("%d. Enter employee name : ", i);
-        scanf("%s", e1[i].name);
+        scanf("%s", e[i].name);
         printf("%d. Enter employee code : ", i);
-        scanf("%d", &e1[i].code);
+        scanf("%d", &e[i].code);
         printf("%d. Enter employee salary : ", i);
-        scanf("%f", &e1[i].salary);
+        scanf("%f", &e[i].salary);
         printf("\n");
     }
     // Display the entered details
     printf("Entered Employee Details:\n");
     for (int i = 1; i < 4; i++)
     {
-        printf("%d. Name: %s, Code: %d, Salary: %.2f\n", i + 1, e1[i].name, e1[i].code, e1[i].salary);
+        printf("%d. Name: %s, Code: %d, Salary: %.2f\n", i + 1, e[i].name, e[i].code, e[i].salary);
     }
     return 0;
 }
